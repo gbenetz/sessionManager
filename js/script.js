1d25252d37d14ffcc5fe66fd4cd3153eeb16130b
Guido Benetti <guido.benetti01@ateneopv.it>
2018-01-04 17:36:51 +0100

    Removed the export code

--- js/script.js
+++ js/script.js
@@ -420,25 +420,6 @@ function onGot(data) {
 	}
 }
 
-/**
- * Event handler for the export button
- * It handles all the operation required to export the sessions data as a json
- * file named sessions.json in the download directory
- */
-function exportData(ev) {
-	var sJson = window.JSON.stringify(sessions);
-	var url = window.URL.createObjectURL(new Blob([sJson]));
-	var obj = {
-		url: url,
-		filename: "sessions.json"
-	};
-	var downloading = browser.downloads.download(obj);
-	downloading.then((id) => {
-		console.log(`Download started ${id}`);
-	})
-	.catch(onError);
-}
-
 /*
  * Add an eventListener for the load event in order to setup things and show all
  * the already saved session
@@ -449,8 +430,6 @@ window.addEventListener("load", (e) => {
 	 */
 	var saveBtn = document.getElementById("session-save");
 	saveBtn.addEventListener("click", saveSession);
-	var exportBtn = document.getElementById("export-cmd");
-	exportBtn.addEventListener("click", exportData);
 	/*
 	 * Load the already saved session (if any)
 	 */
