7526a1ef69e965a4bb50ea7582e980e6f2b63fb8
Guido Benetti <guido.benetti01@ateneopv.it>
2019-08-07 19:10:12 +0200

    Move duplicate createButton to a new common file

--- js/edit.js
+++ js/edit.js
@@ -111,26 +111,6 @@ function createIcon(type, width, height) {
 }
 
 /**
- * Creates a button
- *
- * type is a label that is applied to the class name
- * cmd is the type of command. This two parameters are used to create the class
- * of the new button in the form of 'type'-cmd 'cmd'-btn
- * content is an HTMLElement to be used as the "label" of the button
- * tooltip is the button tooltip text
- * Returns:
- * the newly created button
- */
-function createButton(type, cmd, content, tooltip) {
-	var btn = document.createElement("button");
-	btn.className = type + "-cmd " + cmd + "-btn";
-	btn.appendChild(content);
-	btn.title = tooltip;
-	return btn;
-
-}
-
-/**
  * Creates a row for tab data
  *
  * tab is an object containing:
--- js/script.js
+++ js/script.js
@@ -5,25 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 var sessions = [];
-/**
- * Creates a button
- *
- * type is a label that is applied to the class name
- * cmd is the type of command. This two parameters are used to create the class
- * of the new button in the form of 'type'-cmd 'cmd'-btn
- * content is an HTMLElement to be used as the "label" of the button
- * tooltip is the button tooltip text
- * Returns:
- * the newly created button
- */
-function createButton(type, cmd, content, tooltip) {
-	var btn = document.createElement("button");
-	btn.className = type + "-cmd " + cmd + "-btn";
-	btn.appendChild(content);
-	btn.title = tooltip;
-	return btn;
-
-}
 
 /**
  * Create an img with the specified icon and size
